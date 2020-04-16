28aec8a4dede069fc99659221f497eb1b5214f69
Juan Gómez <j.gtrapero@gmail.com>
2020-04-16 12:05:08 +0200

    Created use case to get all episodes

---
+++ domain/src/main/java/com/juangomez/domain/usecases/GetEpisodes.kt
@@ -0,0 +1,13 @@
+package com.juangomez.domain.usecases
+
+import com.juangomez.common.Either
+import com.juangomez.common.Failure
+import com.juangomez.domain.models.Episodes
+import com.juangomez.domain.repositories.EpisodeRepository
+
+class GetEpisodes(private val episodeRepository: EpisodeRepository) :
+    BaseUseCase<Episodes, BaseUseCase.None>() {
+
+    override suspend fun run(params: None?): Either<Failure, Episodes> =
+        episodeRepository.getEpisodes()
+}
