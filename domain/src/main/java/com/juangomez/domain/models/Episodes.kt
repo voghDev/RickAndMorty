3fd594143e25994a1ad26293cd54d6950bcaeb4b
Juan Gómez <j.gtrapero@gmail.com>
2020-04-16 12:41:46 +0200

    Group episodes by season

--- domain/src/main/java/com/juangomez/domain/models/Episodes.kt
+++ domain/src/main/java/com/juangomez/domain/models/Episodes.kt
@@ -1,3 +1,11 @@
 package com.juangomez.domain.models
 
-data class Episodes(val episodes: List<Episode>)
+data class Episodes(val episodes: List<Episode>) {
+
+    fun groupBySeasons(): Seasons = episodes
+        .groupBy { it.season }
+        .map { Season(it.key, it.value.toEpisodes()) }
+        .toSeasons()
+}
+
+fun List<Episode>.toEpisodes() = Episodes(this)
