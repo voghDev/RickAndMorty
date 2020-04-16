78dbed33280d369c112d8276666bd18d66bfe722
Juan Gómez <j.gtrapero@gmail.com>
2020-04-16 11:21:14 +0200

    Included remote failures

--- common/src/main/java/com/juangomez/common/Failure.kt
+++ common/src/main/java/com/juangomez/common/Failure.kt
@@ -1,5 +1,7 @@
 package com.juangomez.common
 
 sealed class Failure(val exception: Exception = Exception("Failure")) {
-    
+    object NetworkConnection : Failure()
+    object ServerError : Failure()
+    object UnknownRemoteError : Failure()
 }
