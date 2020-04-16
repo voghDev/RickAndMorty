5e72568efda5335bb643fc4f13e7e277df1ba45d
Juan Gómez <j.gtrapero@gmail.com>
2020-04-16 12:04:52 +0200

    Return Episodes in repository

--- domain/src/main/java/com/juangomez/domain/repositories/EpisodeRepository.kt
+++ domain/src/main/java/com/juangomez/domain/repositories/EpisodeRepository.kt
@@ -1,4 +1,10 @@
 package com.juangomez.domain.repositories
 
+import com.juangomez.common.Either
+import com.juangomez.common.Failure
+import com.juangomez.domain.models.Episodes
+
 interface EpisodeRepository {
+
+    suspend fun getEpisodes(): Either<Failure, Episodes>
 }
