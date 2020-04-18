65dddfc074532abd400d43b2b9254545098680a6
Juan Gómez <j.gtrapero@gmail.com>
2020-04-18 09:50:42 +0200

    Added tests

--- cache/src/main/java/com/juangomez/cache/EpisodeCacheProviderImpl.kt
+++ cache/src/main/java/com/juangomez/cache/EpisodeCacheProviderImpl.kt
@@ -7,11 +7,13 @@ class EpisodeCacheProviderImpl : EpisodeCacheProvider {
 
     private var episodes = mutableListOf<Episode>()
 
+    override fun getEpisodes() = episodes
+
     override fun setEpisodes(episodes: List<Episode>) {
         this.episodes = episodes.toMutableList()
     }
 
-    override fun getEpisode(id: Int): Episode? = episodes.find { episode -> episode.id == id }
+    override fun getEpisode(id: Int) = episodes.find { episode -> episode.id == id }
 
     override fun setEpisode(episode: Episode) {
         if(getEpisode(episode.id) == null) episodes.add(episode)
--- data/src/main/java/com/juangomez/data/providers/cache/EpisodeCacheProvider.kt
+++ data/src/main/java/com/juangomez/data/providers/cache/EpisodeCacheProvider.kt
@@ -4,6 +4,8 @@ import com.juangomez.domain.models.Episode
 
 interface EpisodeCacheProvider {
 
+    fun getEpisodes(): List<Episode>?
+
     fun setEpisodes(episodes: List<Episode>)
 
     fun getEpisode(id: Int): Episode?
