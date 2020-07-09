dafd53a85fdc01b6df23acf4696a9a2f5cba0517
Olmo Gallegos <vendofiatpunto@hotmail.com>
2020-07-09 16:31:47 +0200

    Make next page nullable

--- remote/src/main/java/com/juangomez/remote/models/RemotePaginationInfo.kt
+++ remote/src/main/java/com/juangomez/remote/models/RemotePaginationInfo.kt
@@ -6,13 +6,13 @@ import okhttp3.HttpUrl
 data class RemotePaginationInfo(
     @SerializedName("count") val count: Int,
     @SerializedName("pages") val pages: Int,
-    @SerializedName("next") val next: String,
-    @SerializedName("prev") val prev: String
+    @SerializedName("next") val next: String?,
+    @SerializedName("prev") val prev: String?
 ) {
 
     companion object {
         private const val PAGE_QUERY_PARAMETER_NAME = "page"
     }
 
-    fun getNextPage() = HttpUrl.parse(next)?.queryParameter(PAGE_QUERY_PARAMETER_NAME)?.toInt()
+    fun getNextPage() = HttpUrl.parse(next ?: "")?.queryParameter(PAGE_QUERY_PARAMETER_NAME)?.toInt()
 }
