abb8d13fa835897e575d4bf3e695043bd20fdc1f
Olmo Gallegos <vendofiatpunto@hotmail.com>
2020-09-30 12:20:24 +0200

    Change test to check VM state

--- presentation/src/test/java/com/juangomez/rickandmorty/views/seasons/SeasonsViewModelTest.kt
+++ presentation/src/test/java/com/juangomez/rickandmorty/views/seasons/SeasonsViewModelTest.kt
@@ -5,6 +5,7 @@ import com.juangomez.domain.usecases.GetSeasonsUseCase
 import io.mockk.MockKAnnotations
 import io.mockk.mockk
 import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,11 +20,11 @@ class SeasonsViewModelTest {
     fun setup() = MockKAnnotations.init(this)
 
     @Test
-    fun `should be able to init a view model without errors`() {
+    fun `should transact to Loading state when ViewModel is initialized`() {
         val viewModel = SeasonsViewModel(mockUseCase)
 
         viewModel.initialState()
 
-        assertNotNull(viewModel)
+        assertTrue(viewModel.state.value is SeasonsViewModel.State.Loading)
     }
 }
