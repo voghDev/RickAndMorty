5c8b4f2c744e0a81ed79d1d61f5f3ae6d6bc5ae8
Olmo Gallegos <vendofiatpunto@hotmail.com>
2020-09-30 12:17:20 +0200

    Fix Unit tests

--- presentation/src/test/java/com/juangomez/rickandmorty/views/seasons/SeasonsViewModelTest.kt
+++ presentation/src/test/java/com/juangomez/rickandmorty/views/seasons/SeasonsViewModelTest.kt
@@ -1,15 +1,25 @@
 package com.juangomez.rickandmorty.views.seasons
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.juangomez.domain.usecases.GetSeasonsUseCase
-import com.juangomez.rickandmorty.MockEpisodeRepository
+import io.mockk.MockKAnnotations
+import io.mockk.mockk
 import junit.framework.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SeasonsViewModelTest {
-    private val mockUseCase = GetSeasonsUseCase(MockEpisodeRepository())
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val mockUseCase = mockk<GetSeasonsUseCase>()
+
+    @Before
+    fun setup() = MockKAnnotations.init(this)
 
     @Test
-    fun `view model should be testable`() {
+    fun `should be able to init a view model without errors`() {
         val viewModel = SeasonsViewModel(mockUseCase)
 
         viewModel.initialState()
