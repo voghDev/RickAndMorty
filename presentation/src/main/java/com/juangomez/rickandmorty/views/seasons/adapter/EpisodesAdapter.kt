0589c757a969c8c35b9ab986ac11127e93594e34
Olmo Gallegos <vendofiatpunto@hotmail.com>
2021-03-28 17:24:03 +0200

    Migrate Adapter to ViewBinding

--- presentation/src/main/java/com/juangomez/rickandmorty/views/seasons/adapter/EpisodesAdapter.kt
+++ presentation/src/main/java/com/juangomez/rickandmorty/views/seasons/adapter/EpisodesAdapter.kt
@@ -4,15 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.juangomez.domain.models.Episode
-import com.juangomez.rickandmorty.R
+import com.juangomez.rickandmorty.databinding.EpisodeRowBinding
 import com.juangomez.rickandmorty.views.seasons.adapter.viewholder.EpisodesViewHolder
 
 class EpisodesAdapter(private val episodes: List<Episode>) :
     RecyclerView.Adapter<EpisodesViewHolder>() {
 
+    private lateinit var binding: EpisodeRowBinding
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodesViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.episode_row, parent, false)
-        return EpisodesViewHolder(view)
+        binding = EpisodeRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EpisodesViewHolder(binding.root, binding)
     }
 
     override fun onBindViewHolder(holder: EpisodesViewHolder, position: Int) =
--- presentation/src/main/java/com/juangomez/rickandmorty/views/seasons/adapter/viewholder/EpisodesViewHolder.kt
+++ presentation/src/main/java/com/juangomez/rickandmorty/views/seasons/adapter/viewholder/EpisodesViewHolder.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.juangomez.domain.models.Episode
+import com.juangomez.rickandmorty.databinding.EpisodeRowBinding
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.episode_row.view.*
 
-class EpisodesViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+class EpisodesViewHolder(
+    override val containerView: View,
+    private val binding: EpisodeRowBinding
+) : RecyclerView.ViewHolder(containerView),
     LayoutContainer {
 
     @SuppressLint("SetTextI18n")
     fun bind(episode: Episode) {
-        containerView.episodeNameText.text = episode.name
-        containerView.episodeNumberText.text = "#${episode.number}"
+        binding.episodeNameText.text = episode.name
+        binding.episodeNumberText.text = "#${episode.number}"
     }
 }
